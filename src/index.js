3f295b6b4f51dffbc5d168eac7f61bcd57315915
Marcio Flavio <mflaviof1995@gmail.com>
2022-01-31 11:36:56 -0300

    Initial simple counter application

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  return (
+    <section className="App">
+      <h1>Contador</h1>
+      <div>
+        <div>0</div>
+        <button>Incrementar</button>
+        <button>Decrementar</button>
+      </div>
+    </section>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(<App />, document.querySelector("#root"));

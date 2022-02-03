cd22ca31741db0ea69952c3a2a61fe36e5b08ee2
Marcio Flavio <mflaviof1995@gmail.com>
2022-02-03 11:55:23 -0300

    Now it renders with jxt too

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-// import React from "react";
-// import { render } from "react-dom";
-
 function convertToHtml(virtualNode) {
-  const $domElement = document.createElement(virtualNode.tagName);
+  const $domElement = virtualNode.tagName
+    ? document.createElement(virtualNode.tagName)
+    : document.createElement(virtualNode.type);
 
   if (typeof virtualNode === "string" || typeof virtualNode === "number") {
     return document.createTextNode(`${virtualNode}`);
   }
 
-  virtualNode.props.children.forEach((virtualChild) => {
+  const children = [...virtualNode.props.children];
+
+  children.forEach((virtualChild) => {
     $domElement.appendChild(convertToHtml(virtualChild));
   });
 
@@ -56,6 +57,17 @@ function App() {
       React.createElement("button", null, "Decrementar")
     )
   );
+
+  // return (
+  //   <section className="App">
+  //     <h1>Contador</h1>
+  //     <div>
+  //       <div>0</div>
+  //       <button>Incrementar</button>
+  //       <button>Decrementar</button>
+  //     </div>
+  //   </section>
+  // );
 }
 
 render(React.createElement(App, null), document.querySelector("#root"));
